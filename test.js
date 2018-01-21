23cf21f245ddaeb71ed9dc030e687e1c94e8d1b8
kobi-cohen <kobimagic@gmail.com>
2018-01-21 11:09:13 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,36 @@
+console.log("Start our Script ! : ");
+console.log("=====================");
+
+var content=document.getElementById('content');
+//var btn=document.getElementById('btn1').addEventListener("click");
+var c={	name:"kobi",
+		family:"Co",
+		age:"32",
+		id:"0007222"
+};
+console.log(c.name);
+funcion(){
+content.innerHtml=c.age;};
+//content.innerHtml=c.name;
+
+//content.innerHtml="Hello World";
+
+		// getInfo:funcion(){
+		// 	return this;
+		// }} ; 
+			
+		// 	 console.log(
+		
+		// 	this.name[x+1] +" "+
+		// 	this.family[x+1]+ " "+
+		// 	this.age[x+1]+ " "+
+		// 	this.id[x+1]));
+		
+
+		// };
+
+
+// function check(){
+// var txt=document.getElementById('txt1').value;
+// content.innerHtml=c.getInfo(txt);
+// }
